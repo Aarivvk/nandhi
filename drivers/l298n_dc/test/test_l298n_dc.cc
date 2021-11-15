4d33bc987f5716aa212635aa100aa5dddbdbcac4
VK <vinaya.rvce@gmail.com>
2021-11-15 06:40:32 +0000

    added back wheels

--- drivers/l298n_dc/test/test_l298n_dc.cc
+++ drivers/l298n_dc/test/test_l298n_dc.cc
@@ -13,22 +13,25 @@ void signal_cb(int num)
 int main()
 {
         signal(SIGINT, signal_cb);
-        L298n_dc motor_driver(1, 4, 5, 26, 31, 11);
-        int16_t speed = 435;
+        L298n_dc motor_driver_front(1, 4, 5, 26, 31, 11);
+        L298n_dc motor_driver_back(23, 0, 2, 24, 3, 12);
+        int16_t speed = 600;
         bool togel = false;
         while (run)
         {
-                std::cout << "\r" << "Speed " << speed  << std::endl;
-                motor_driver.setSpeed(speed, speed);
-                motor_driver.forward();
+                std::cout << "\r" << "Speed " << speed  << std::flush;
+                // motor_driver_front.setSpeed(speed, speed);
+                // motor_driver_front.forward();
+                // motor_driver_back.setSpeed(speed, speed);
+                // motor_driver_back.forward();
                 delay(100);
-                // motor_driver.reverse();
+                // motor_driver_front.reverse();
                 // delay(3000);
-                // motor_driver.clockwiseDiff();
+                // motor_driver_front.clockwiseDiff();
                 // delay(3000);
-                // motor_driver.antiClockwiseDiff();
+                // motor_driver_front.antiClockwiseDiff();
                 // delay(3000);
-                // motor_driver.stop();
+                // motor_driver_front.stop();
 
                 if(1023 == speed || 434 == speed)
                 {
@@ -36,14 +39,19 @@ int main()
                 }
                 if(togel)
                 {
+                        motor_driver_front.forward();
+                        motor_driver_back.forward();
                         speed--;
                 }
                 else
                 {
+                        motor_driver_front.reverse();
+                        motor_driver_back.reverse();
                         speed++;
                 }
 
                 
         }
-        motor_driver.stop();
+        motor_driver_front.stop();
+        motor_driver_back.stop();
 }
