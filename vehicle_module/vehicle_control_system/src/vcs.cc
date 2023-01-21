fd329b26020e93d458f6e5dd189d99bc1118497f
VK <vinaya.rvce@gmail.com>
2023-01-21 19:22:10 +0000

    Fix vehicle_control_system package
    - disabled the test, yet make good test

--- vehicle_module/vehicle_control_system/src/vcs.cc
+++ vehicle_module/vehicle_control_system/src/vcs.cc
@@ -6,7 +6,6 @@
 
 #include "i2c_pwm_pca9685/i2c_pwm.hh"
 
-
 using std::placeholders::_1;
 
 class AckermanCMDReciver : public rclcpp::Node
@@ -21,13 +20,8 @@ public:
         m_pca.setPWM(0, servo_mid);
     }
 
-    AckermanCMDReciver()
-    {
-        m_pca.setPWM(1, servo_mid);
-        m_pca.setPWM(0, servo_mid);
-    }
-
-        private : void topic_callback(const nandhi_msg_types::msg::AckermannDrive &msg) const
+private:
+    void topic_callback(const nandhi_msg_types::msg::AckermannDrive &msg)
     {
         RCLCPP_INFO(this->get_logger(), "I heard: '%s'", std::to_string(msg.speed).c_str());
         m_pca.setPWM(0, msg.speed);
@@ -35,7 +29,7 @@ public:
     }
     rclcpp::Subscription<nandhi_msg_types::msg::AckermannDrive>::SharedPtr m_subscription;
     PCA9685 m_pca = PCA9685(8, 0x40);
-    constexpr int servo_mid = 340;
+    const int servo_mid = 340;
 };
 
 int main(int argc, char *argv[])
