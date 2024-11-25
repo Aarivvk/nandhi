3ba04381afbba8fb8ec4a356eeefab8e0067c2f5
aarrivvk <8676836+Aarivvk@users.noreply.github.com>
2024-11-25 08:00:53 +0100

    working lidar

--- simulation/nandhi_sim_launch/launch/nandhi_sim.launch.py
+++ simulation/nandhi_sim_launch/launch/nandhi_sim.launch.py
@@ -30,10 +30,10 @@ def generate_launch_description():
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': '-r '+os.path.join(
+        launch_arguments={'gz_args': '-r -s '+os.path.join(
             pkg_project_gazebo,
             'worlds',
-            'indoor.sdf'
+            'industrial-warehouse.sdf'
         )}.items(),
     )
 
